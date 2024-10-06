b4163456d47c610fdc0f1d6f4ebd33210a87c93f
siddarthsingotam <siddarthsingotam1213@gmail.com>
2024-10-06 19:59:21 +0300

    Lab11 init

---
+++ app/lab11/src/main/java/com/example/lab11/MainActivity.kt
@@ -0,0 +1,231 @@
+package com.example.lab11
+
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.lab11.ui.theme.Andriod_LabsTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.UUID
+
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import android.bluetooth.BluetoothDevice
+
+
+class MyViewModel : ViewModel() {
+    companion object GattAttributes {
+        const val SCAN_PERIOD: Long = 3000
+        const val STATE_DISCONNECTED = 0
+        const val STATE_CONNECTING = 1
+        const val STATE_CONNECTED = 2
+        val UUID_HEART_RATE_MEASUREMENT = UUID.fromString("00002a37-0000-1000-8000-00805f9b34fb")
+        val UUID_HEART_RATE_SERVICE = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb")
+        val UUID_CLIENT_CHARACTERISTIC_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
+    }
+
+    val scanResults = MutableLiveData<List<ScanResult>>(null)
+    val fScanning = MutableLiveData<Boolean>(false)
+    private val mResults = java.util.HashMap<String, ScanResult>()
+
+    fun scanDevices(scanner: BluetoothLeScanner, context: Context) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (context.checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    fScanning.postValue(true)
+                    val settings = ScanSettings.Builder()
+                        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                        .setReportDelay(0)
+                        .build()
+                    scanner.startScan(null, settings, leScanCallback)
+                    delay(SCAN_PERIOD)
+                    scanner.stopScan(leScanCallback)
+                    scanResults.postValue(mResults.values.toList())
+                    fScanning.postValue(false)
+                } catch (e: SecurityException) {
+                    Log.e("DBG", "Permission not granted for Bluetooth scan", e)
+                }
+            } else {
+                Log.e("DBG", "Bluetooth scan permission not granted")
+            }
+        }
+    }
+
+    val leScanCallback: ScanCallback = object : ScanCallback() {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            val device: BluetoothDevice = result.device
+            val deviceAddress = device.address
+            mResults[deviceAddress] = result
+            Log.d("DBG", "Device address: $deviceAddress (${result.isConnectable})") // build.gradle: minSdk = 26
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ShowDevices(mBluetoothAdapter: BluetoothAdapter, model: MyViewModel = viewModel()) {
+    val context = LocalContext.current
+    val value: List<ScanResult>? by model.scanResults.observeAsState(null)
+    val fScanning: Boolean by model.fScanning.observeAsState(false)
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { model.scanDevices(mBluetoothAdapter.bluetoothLeScanner, context) },
+            enabled = !fScanning,
+            modifier = Modifier.padding(8.dp).height(64.dp).width(144.dp)
+        ) {
+            Text(if (fScanning) "Scanning" else "Scan Now")
+        }
+
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = 1.dp,
+            color = Color.Gray
+        )
+
+        if (value.isNullOrEmpty()) {
+            Text(text = "No devices found", modifier = Modifier.padding(8.dp))
+        }
+        else {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                LazyColumn(modifier = Modifier.fillMaxSize().padding(2.dp)) {
+                    items(value ?: emptyList()) { result ->
+                        val deviceName = result.device.name ?: "UNKNOWN"
+                        val deviceAddress = result.device.address
+                        val deviceStrength = result.rssi
+
+                        Row(
+                            modifier = Modifier.padding(2.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = "${deviceStrength}dBm",
+                                modifier = Modifier.padding(end = 10.dp).align(Alignment.CenterVertically)
+                            )
+
+                            Column(
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                Text(
+                                    text = deviceName,
+                                    modifier = Modifier.padding(4.dp),
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis,
+                                    fontWeight = FontWeight.Bold,
+                                )
+                                Text(text = deviceAddress)
+                            }
+
+                            Button(
+                                enabled = result.isConnectable(),
+                                onClick = { Log.d("DBG", "Clicked ${result.device.address} ${result.device.name}") },
+                                modifier = Modifier.padding(8.dp).align(Alignment.CenterVertically)
+                            ) {
+                                Text("Connect")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class MainActivity : ComponentActivity() {
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun hasPermissions(): Boolean {
+        if (mBluetoothAdapter == null || !mBluetoothAdapter!!.isEnabled) {
+            Log.e("DBG", "No Bluetooth LE capability")
+            return false
+        }
+        else if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
+            checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED ||
+            checkSelfPermission(Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_ADMIN), 1);
+        }
+        return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        mBluetoothAdapter = bluetoothManager.adapter
+
+        setContent {
+            Andriod_LabsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Log.i("DBG", "Device has Bluetooth support: ${hasPermissions()}")
+                        when {
+                            mBluetoothAdapter == null       -> Text("Bluetooth is not supported on this device")
+                            !mBluetoothAdapter!!.isEnabled  -> Text("Bluetooth is turned off")
+                            else                            -> ShowDevices(mBluetoothAdapter!!, viewModel())
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
